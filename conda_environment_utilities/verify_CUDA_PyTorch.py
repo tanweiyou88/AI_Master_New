870508d521e58e2902b865c467e5817fcbb3ba63
Wei You Tan <weiyou01@hotmail.com>
2024-10-08 03:17:24 +0800

    Latest version

---
+++ conda_environment_utilities/verify_CUDA_PyTorch.py
@@ -0,0 +1,25 @@
+import torch
+
+# To print Cuda version
+print('The CUDA version for this PyTorch:', torch.version.cuda)
+
+# To check whether CUDA is supported
+print('Whether CUDA is supported by our system:', torch.cuda.is_available())
+
+# To print the number of GPUs available
+print('The number of GPUs available:', torch.cuda.device_count())
+
+print()
+
+
+# setting device on GPU if available, else CPU
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('Using device:', device)
+
+#Additional Info when using cuda
+if device.type == 'cuda':
+    print('Name of the device:', torch.cuda.get_device_name(0))
+    print('Device ID:', torch.cuda.current_device())
+    print('Memory Usage:')
+    print('\tAllocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
+    print('\tCached:   ', round(torch.cuda.memory_reserved(0)/1024**3,1), 'GB')
